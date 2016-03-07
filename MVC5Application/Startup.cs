545dc12bf22bb7912e41f45e0305c2d573a2e7ad
Wayne <kuanchieh1102@hotmail.com>
2016-03-07 23:27:57 +0800

    initial commit

---
+++ MVC5Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5Application.Startup))]
+namespace MVC5Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
